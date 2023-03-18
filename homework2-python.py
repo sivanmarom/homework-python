d52c5420d40f1843b60d3c75f248859e18b3fd98
sivan marom <sivmamrom@gmail.com>
2023-03-18 14:01:05 +0200

    homework modified

--- homework2-python.py
+++ homework2-python.py
@@ -1,9 +1,10 @@
-# Q-1
+# Q-1  Create a list with your identification information.
+# For example my lists length will be 4. If I will reach the value of the list on index 0, list[0] I will receive the “Alex”. Run with for loop over the list and print its content.
 identification_list = ["Sivan", "Marom", 28, "0542071277"]
 for val in identification_list:
     print(val)
 
-# Q-2
+# Q-2 Create a program that receives two lists of the same length with integers as values, if the lists are not of the same length the program must return the relevant error. The program will create a new list that will contain the largest value between the two lists at a specific index.
 def higher_value(_list1, _list2):
     higher_list = []
     assert len(_list1) == len(_list2), "Your list's length is not the same!"
@@ -16,7 +17,7 @@ def higher_value(_list1, _list2):
 
 print(higher_value([1,2,3,4,5], [5,4,3,2,1]))
 
-#  Q-3
+#  Q-3  Create a python program that will count the number of appearances of odd and even values in a list. In case that the program encounters a string use break statement and return a print that says, “It’s a string!!!” and nullify the values of odd and even numbers counters.
 def count_odds_and_even(input_list):
     odd_counter = 0
     even_counter = 0
@@ -36,7 +37,7 @@ def count_odds_and_even(input_list):
     print("Numbers of odd numbers:", odd_counter)
 count_odds_and_even( [1, 2, 3, 4, 5, 8, 9])
 
-# Q-4
+# Q-4  Build a function that will receive a list with integer values as its input. The function will calculate the sum of the integers in the list and will return the sum result.
 def sum_list(input_list):
     sum_result = 0
     for val in input_list:
@@ -45,7 +46,7 @@ def sum_list(input_list):
 
 print(sum_list([1,2,3,4]))
 
-# Q-5
+# Q-5  Build a function that will receive a list with integer values as its input. The function will multiply all values in the list and return the result.
 def multiplication_list(input_list):
     multiply_result = 1
     for val in input_list:
@@ -54,7 +55,7 @@ def multiplication_list(input_list):
 
 print(multiplication_list([1,2,3,4]))
 
-# Q-6
+# Q-6  Build a function that will receive a list with integer values as its input. The function will find the minimal value in the list and return this value.
 def find_min_value(input_list):
     min_value = input_list[0]
     for val in input_list:
@@ -63,7 +64,7 @@ def find_min_value(input_list):
     return min_value
 print(find_min_value([6,2,3,4,5]))
 
-# Q-7
+# Q-7  Build a Python function that accepts a string and calculate the number of upper case letters and lower case letters.
 def lower_and_upper(input_str):
     _upper_counter = 0
     _lower_counter = 0
@@ -80,7 +81,7 @@ def lower_and_upper(input_str):
 
 lower_and_upper("Alex Kuznetsov Hello")
 
-# Q-8
+# Q-8  Build a Python function that takes a list and returns a new list with unique elements of the first list.
 def unique_elem(input_list):
     unique_set = set(input_list)
     unique_list = list(unique_set)
@@ -88,7 +89,7 @@ def unique_elem(input_list):
 
 print(unique_elem([1, 2, 3, 3, 3, 3, 4, 5]))
 
-# Q-9
+# Q-9  Write a Python program to construct the following pattern, using a nested loop.
 def pattern_1():
     for i in range(1,9):
         for j in range(1,i+1):
@@ -97,7 +98,7 @@ def pattern_1():
 
 pattern_1()
 
-# Q-10
+# Q-10  Write a Python program to print the following patterns
 def pattern_2():
     for i in range(7):
         for j in range(5):
